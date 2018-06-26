5e21ae5e225466537cff0b313d5bb262bae1837a
teeaaspoon <tommypoon36@gmail.com>
2018-06-26 16:47:56 +0000

    moved script to lookup_people.js

---
+++ lookup_people.js
@@ -0,0 +1,44 @@
+const pg = require("pg");
+const settings = require("./settings");
+const moment = require("moment");
+
+const input = process.argv[2];
+const capitalizedInput = input.charAt(0).toUpperCase() + input.slice(1);
+
+const client = new pg.Client({
+    user: settings.user,
+    password: settings.password,
+    database: settings.database,
+    host: settings.hostname,
+    port: settings.port,
+    ssl: settings.ssl
+});
+
+client.connect(err => {
+    if (err) {
+        return console.error("Connection Error", err);
+    }
+    client.query(
+        `SELECT * 
+                FROM famous_people 
+                WHERE first_name = '${capitalizedInput}'`,
+        (err, result) => {
+            if (err) {
+                return console.error("error running query", err);
+            }
+            console.log("Searching ...");
+            console.log(`Found ${result.rows.length} person(s) by the name '${capitalizedInput}':`);
+            resultingString(result.rows)
+            
+            client.end();
+        }
+    );
+});
+
+function resultingString(arrayOfObjects) {
+    let counter = 1;
+    arrayOfObjects.forEach(person => {
+        console.log(`- ${counter}: ${person["first_name"]} ${person["last_name"]}, born '${moment(person["birthdate"]).format('YYYY/MM/DD')}'`);
+        counter += 1;
+    });
+}
